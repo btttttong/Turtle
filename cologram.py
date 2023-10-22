e96dd469b830182ba9c33dc32e2b9298d8fe3f4f
Supak Rodklang <tong.7147@gmail.com>
2023-10-22 17:21:11 +0700

    done

--- cologram.py
+++ cologram.py
@@ -1,35 +1,21 @@
+import turtle as t
 import colorgram
-from turtle import Turtle
-
-t = Turtle()
 
 colors = colorgram.extract('spots.jpg', 25)
 
-print(colors)
-# colorgram.extract returns Color objects, which let you access
-# RGB, HSL, and what proportion of the image was that color.
-first_color = colors[0]
-rgb = first_color.rgb  # e.g. (255, 151, 210)
-hsl = first_color.hsl  # e.g. (230, 255, 203)
-proportion = first_color.proportion  # e.g. 0.34
+t.colormode(255)
+tim = t.Turtle()
+tim.hideturtle()
+tim.penup()
+INIT_POS = (-150,300)
+tim.goto(INIT_POS)
 
 for i in range(len(colors)):
-    print(f'{colors[i].rgb.r},{colors[i].rgb.g},{colors[i].rgb.b}')
-    tupi = (float(colors[i].rgb.r), float(colors[i].rgb.g), float(colors[i].rgb.b))
-    print(tupi)
-    print(len(colors))
-    if i % 5 != 0:
-        t.color()
-        t.dot(20, tupi)
-        t.fd(100)
+    print(i)
+    if i % 5 == 0:
+        tim.goto(INIT_POS[0] , tim.ycor()-100)
     else:
-        t.right(90)
+        tim.fd(100)
+    tim.dot(50, (int(colors[i].rgb.r), int(colors[i].rgb.g), int(colors[i].rgb.b)))
 
-
-t.screen.mainloop()
-# RGB and HSL are named tuples, so values can be accessed as properties.
-# These all work just as well:
-# red = rgb[0]
-# red = rgb.r
-# saturation = hsl[1]
-# saturation = hsl.s
+tim.screen.mainloop()
